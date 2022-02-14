db4d413a04572efbf1218f85f4f617c806a914c0
richasare <asarerichemond@gmail.com>
2022-02-14 17:51:56 +0100

    La navbar

---
+++ navbar.php
@@ -0,0 +1,20 @@
+<div class="container">
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="#"><!--Non de l'app--> "Projet Fichier"</a>
+   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav ms-auto">
+      <li class="nav-item active">
+        <a  id="connect" class="nav-link" href="#">Connexion</a>
+      </li>
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Inscription</a>
+      </li>
+
+  </div>
+
+</nav>
+</div>
