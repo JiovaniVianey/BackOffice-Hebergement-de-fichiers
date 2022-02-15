d2158bacc2521c628d3832632a26506e12812dd7
valentinfo <91597704+valentinfo@users.noreply.github.com>
2022-02-15 04:20:47 +0100

    Add files via upload
    espace de connexion

---
+++ vue/ConnexionUtil.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html>
+	<head>
+		<title>WebSite</title>
+		<meta charset='utf-8'>
+		<link rel='stylesheet' href='style.css'>
+	</head>
+	<body>
+		
+		<form class="box" method="POST" action="login.html">
+		<h1>LOGIN</h1>
+		 <input type='text' name='pseudo' placeholder="Username" required="">
+		<input type='password' name='pseudo' placeholder="Password" required="">
+		
+		
+		
+		
+		
+		<input type="submit" value="Login"/>
+		</form>
+	</body>
+</html>	
