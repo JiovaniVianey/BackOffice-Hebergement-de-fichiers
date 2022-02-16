fbe2177a3fdbfbf9f55955e955882d93da5616bd
KingPigGameStudios / KPGS <jiovani.vianey@gmail.com>
2022-02-16 19:31:19 +0100

    Update controllerAdmin.php

---
+++ controller/controllerAdmin.php
@@ -0,0 +1,19 @@
+<?php
+
+$action = $_GET["action"] ;
+
+    switch($action){
+        case "autorlecture":
+            Admin::autorLecture($_POST["choixutil"]);
+            break;
+        case "autorModif":
+            Admin::autorModif($_POST["choixutil"]);
+            break;
+        case "suppression":
+            Admin::supprUtil($_POST["choixutil"]);
+            break;
+        default:
+            Admin::accesUtil($_POST["choixutil"]);
+            break;
+    }
+?>
