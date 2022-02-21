f8f5d601de094d51a879b4910a9490b5d712c2db
valentinfo <91597704+valentinfo@users.noreply.github.com>
2022-02-21 04:30:19 +0100

    Add files via upload
    Salle d'attente en attendant d'être validé par Goran (dsl du retard mdr)

---
+++ vue/Attente.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html>
+	<head>
+		<title>WebSite</title>
+		<meta charset='utf-8'>
+		<link rel='stylesheet' href='../style/style.css'>
+	</head>
+    <body>
+
+        <center>
+        <img class="imgsad" src="../images/sad3v2.gif" alt="image de sablier">
+        <h3>L'administrateur vous donnera l'accès très prochainement.</h3>
+</center>
+    </body>
