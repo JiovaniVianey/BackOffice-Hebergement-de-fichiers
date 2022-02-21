de6f87d96fc9a633551f627f368a43d37f812111
Tom.bouteselle <tom.bouteselle@gmail.com>
2022-02-21 02:20:33 +0100

    liste_utlisateur vue set up

--- BDD/bdd.php
+++ BDD/bdd.php
@@ -2,7 +2,7 @@
 include_once("parametres.php");
 $param = 'mysql:host='. HOST .';dbname='. DBNAME .';charset=utf8';
 try {
-    $bdd = new PDO($param, USER, PASS);
+    $bdd = new PDO($param, "root", "");
 } catch(Exception $e) {
     echo $e;
 }
--- vue/liste_utilisateurs_vue.php
+++ vue/liste_utilisateurs_vue.php
@@ -9,13 +9,16 @@
     ?>
 <table class="table table-striped">
   <thead>
+
+    <!-- deux lignes d'exemple -->
     <tr>
       <th scope="col">#</th>
       <th scope="col">Prénom</th>
       <th scope="col">Nom</th>
       <th scope="col">Email</th>
       <th scope="col">Dèrnière IP</th>
-      <th scope="col">Autorisation fichiers</th>
+      <th scope="col">Ajout de fichiers</th>
+      <th scope="col">Suppression de fichiers</th>
       <th></th>
     </tr>
   </thead>
@@ -27,32 +30,15 @@
       <td>blablabla@bloblo.com</td>
       <td>@mdo</td>
       <td><input type="checkbox"></td>
-      <td><a href="">Supprimer l'utilisateur</a></td>
-    </tr>
-    <tr>
-      <th scope="row">2</th>
-      <td>Jacob</td>
-      <td>Thornton</td>
-      <td>blablabla@bloblo.com</td>
-      <td>@fat</td>
       <td><input type="checkbox"></td>
       <td><a href="">Supprimer l'utilisateur</a></td>
     </tr>
-    <tr>
-      <th scope="row">3</th>
-      <td>Larry the Bird</td>
-      <td>the bird</td>
-      <td>blablabla@bloblo.com</td>
-      <td>@twitter</td>
-      <td><input type="checkbox"></td>
-      <td><a href="">Supprimer l'utilisateur</a></td>
-    </tr>
-  </tbody>
-</table>
-</div>
+    
 
-
-<?php
+    <!-- faut faire un form avec la boucle à l'interieur. -->
+    <!-- mettre un input type=checkbox, la Value sera composer de l'$id de l'utilisateur de la liste pr savoir quel input concerne quel utilisateur en back-end-->
+    
+    <?php
         foreach ($lesUtilisateurs as $uti)
         {
             ?>
@@ -62,10 +48,17 @@
                 <td><?php echo $uti -> getNom(); ?></td>
                 <td><?php echo $uti -> getEmail(); ?></td>
                 <td><?php echo $uti -> getIp(); ?></td>
-                <td><?php echo $uti -> getId(); ?></td>
+                <td><?php echo $uti -> getAjout(); ?></td>
+                <td><?php echo $uti -> getSup(); ?></td>
               </tr>
 
                          
             <?php
         }
     ?>
+
+  
+  </tbody>
+</table>
+</div>
+
