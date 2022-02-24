c18e737dcdac61f104de4b518d4db5da1d3f74ee
KingPigGameStudios / KPGS <jiovani.vianey@gmail.com>
2022-02-24 15:00:39 +0100

    Update Attente.php

--- vue/Attente.php
+++ vue/Attente.php
@@ -1,13 +1,19 @@
 <!doctype html>
 <html>
 	<head>
-		<title>WebSite</title>
+		<title>Attente...</title>
 		<meta charset='utf-8'>
-		<link rel='stylesheet' href='style/style.css'>
 	</head>
     <body>
         <center>
-        <img class="imgsad" src="../images/sad3v2.gif" alt="image de sablier">
+        <img class="imgsad" src="images/sad3v2.gif" alt="...">
         <h3>L'administrateur vous donnera l'accès très prochainement.</h3>
         </center>
     </body>
+</html>
+
+<?php
+    if ($Utilisateur->getAutoriser() == true){
+        header('index.php');
+    }
+?>
